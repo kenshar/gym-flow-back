96d37bc37dcfa9350a5f8d7db4d0b74e5d531e0b
Branice Simaloi <simaloibranice@gmail.com>
2026-01-21 08:37:53 +0300

    feat: ensuring members list is present

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
 members_data = [
     { "id": 1, "name": "Branice Nashilu", "status": "Active", "plan": "Gold" },
